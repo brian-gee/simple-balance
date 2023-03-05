73813e507e35d6209a3a6e527f2acc8c2f2d438c
Brian Geertsma <brian86753099@gmail.com>
2023-03-04 19:01:17 -0500

    Initial Commit

---
+++ SimpleBalance/SimpleBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBalanceApp.swift
+//  SimpleBalance
+//
+//  Created by brian on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
