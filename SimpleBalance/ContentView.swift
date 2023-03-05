1451d49eca7dfc36bf708618a42320d18b219dda
Brian Geertsma <brian86753099@gmail.com>
2023-03-04 23:49:27 -0500

    basic account display with sandbox working

--- SimpleBalance/ContentView.swift
+++ SimpleBalance/ContentView.swift
@@ -1,21 +1,64 @@
-//
-//  ContentView.swift
-//  SimpleBalance
-//
-//  Created by brian on 3/4/23.
-//
-
 import SwiftUI
+import Foundation
+
+class ViewModel: ObservableObject {
+    @Published var balance: String?
+    
+    func getApi() {
+        let url = URL(string: "https://api.teller.io/accounts/acc_odbahfmgob0etkrmbq000/balances")!
+        let username = "test_token_c4ssr3yuk7cni"
+        let password = ""
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let loginString = "\(username):\(password)".data(using: .utf8)?.base64EncodedString() ?? ""
+        request.setValue("Basic \(loginString)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Error: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                   let available = json["available"] as? String {
+                    DispatchQueue.main.async {
+                        self.balance = available
+                    }
+                } else {
+                    print("Invalid JSON response")
+                }
+            } catch let error {
+                print("Error parsing JSON: \(error.localizedDescription)")
+            }
+        }
+        
+        task.resume()
+    }
+}
 
 struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                HStack {
+                    Text("Available balance:")
+                    Spacer()
+                    if let balance = viewModel.balance {
+                        Text(balance)
+                    } else {
+                        ProgressView()
+                    }
+                }
+            }
+            .navigationTitle("Simple Balance")
+            .onAppear {
+                viewModel.getApi()
+            }
         }
-        .padding()
     }
 }
 
